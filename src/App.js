053371bdcc99f8916fb03279ddf2ef1e6365320b
richardngo16 <richardngo16@gmail.com>
2020-06-17 10:23:27 -0700

    Added LocalForage to save some network calls

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { STORAGE_CONSTANTS } from './constants'
+import localForage from 'localforage'
 
 function App() {
   useEffect(() => {
-    async function fetchAPIevents() {
-      const response = await fetch("https://covid19.mathdro.id/api");
-      const data = await response.json();
-      console.log(data);
+    async function fetchAllCountries() {
+      const countriesInStorage = await localForage.getItem(STORAGE_CONSTANTS.COUNTRIES)
+      if (!countriesInStorage) {
+        // no countries stored, make network call
+        const response = await fetch("https://covid19.mathdro.id/api/countries");
+        const data = await response.json();
+        await setCountriesInStorage(mapCountryData(data.countries))
+      } else {
+        // country list already exists in storage
+        console.log(countriesInStorage)
+      }
     }
-    fetchAPIevents();
+    fetchAllCountries();
   });
   return (
     <div className="App">
@@ -32,3 +41,12 @@ function App() {
 }
 
 export default App;
+
+const mapCountryData = (countries) => {
+  return countries.map(country => {
+    return { label: country.name, value: country.iso2 };
+  })
+}
+const setCountriesInStorage = async (data) => {
+  localForage.setItem(STORAGE_CONSTANTS.COUNTRIES, data)
+}
